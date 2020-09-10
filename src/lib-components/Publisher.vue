2f49b1467262e3e1634152db00728a3e723276dd
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-10 22:39:36 +0200

    Simplified editor

--- src/lib-components/Publisher.vue
+++ src/lib-components/Publisher.vue
@@ -1,86 +1,50 @@
 <template>
-  <div>
-    <div class="container">
-      <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
-        <div>
-          <button
-            class="button is-light"
-            :class="{ 'is-primary': isActive.bold() }"
-            @click="commands.bold"
-          >
-            <b>B</b>
-          </button>
+  <div class="editor">
+    <editor-menu-bar class="commands" :editor="editor" v-slot="{ commands, isActive }">
+      <div>
+        <button :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
+          <b>B</b>
+        </button>
 
-          <button
-            class="button is-light"
-            :class="{ 'is-primary': isActive.italic() }"
-            @click="commands.italic"
-          >
-            <i>I</i>
-          </button>
+        <button :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
+          <i>I</i>
+        </button>
 
-          <button
-            class="button is-light"
-            :class="{ 'is-primary': isActive.code() }"
-            @click="commands.code"
-          >@</button>
-
-          <button
-            class="button is-light"
-            :class="{ 'is-primary': isActive.code_block() }"
-            @click="commands.code_block"
-          >[@]</button>
-        </div>
-      </editor-menu-bar>
-
-      <div class="box">
-        <editor-content :editor="editor" class="editor" />
+        <button :class="{ 'is-active': isActive.blockquote() }" @click="commands.blockquote">
+          Q
+        </button>
       </div>
-    </div>
+    </editor-menu-bar>
+
+    <editor-content :editor="editor" />
   </div>
 </template>
 
 <script>
-import { EditorContent, EditorMenuBar, Editor } from "tiptap";
-import { Bold, Code, CodeBlock, Italic } from "tiptap-extensions";
-import { Renderer } from "prosemirror-to-html-js";
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import { Bold, Italic, Blockquote } from 'tiptap-extensions'
 
 export default {
   components: {
-    EditorContent,
     EditorMenuBar,
+    EditorContent,
   },
   data() {
     return {
-      editor: {},
-      result: "",
-    };
-  },
-  mounted() {
-    const renderer = new Renderer();
-
-    this.editor = new Editor({
-      extensions: [
-        new Bold(),
-        new Code(),
-        new CodeBlock(),
-        new Italic(),
-      ],
-      onUpdate: ({ getJSON }) => {
-        this.result = (getJSON());
-      },
-    });
-
-    console.log(this.editor);
+      editor: new Editor({
+        extensions: [
+          new Bold(),
+          new Italic(),
+          new Blockquote(),
+        ],
+        onUpdate({getJSON}){
+          //
+        }
+      }),
+    }
   },
   beforeDestroy() {
-    this.editor.destroy();
+    this.editor.destroy()
   },
-};
-</script>
-
-<style scoped lang="scss">
-.editor {
-  padding: 10px 20px;
 }
-</style>
+</script>
