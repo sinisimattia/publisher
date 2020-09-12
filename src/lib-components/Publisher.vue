0bd4be1d95bb4352223abb385924543b1231999b
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-12 17:26:21 +0200

    Set button type

--- src/lib-components/Publisher.vue
+++ src/lib-components/Publisher.vue
@@ -2,15 +2,15 @@
   <div class="editor">
     <editor-menu-bar class="commands" :editor="editor" v-slot="{ commands, isActive }">
       <div class="internal">
-        <button :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
+        <button type="button" :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
           <b>B</b>
         </button>
 
-        <button :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
+        <button type="button" :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
           <i>I</i>
         </button>
 
-        <button :class="{ 'is-active': isActive.blockquote() }" @click="commands.blockquote">
+        <button type="button" :class="{ 'is-active': isActive.blockquote() }" @click="commands.blockquote">
           Q
         </button>
       </div>
