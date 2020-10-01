9f76b3d926ea34c94deae07800c4b8a89638ffb4
Mattia Sinisi <mattia@snisni.it>
2020-10-01 09:50:52 +0200

    Added insertion and removal

--- src/lib-components/ListEditor.vue
+++ src/lib-components/ListEditor.vue
@@ -1,12 +1,23 @@
 <template>
 	<div>
-		<ul class="list">
-			<li class="item" v-for="(item, i) in value" :key="i">{{item}}</li>
+		<ul class="editor">
+			<li class="content" v-for="(item, i) in value" :key="i">
+				<span class="item">{{ item }}</span>
+				<span class="commands">
+					<slot name="commands">
+						<button type="button" @click="remove(i)">Remove</button>
+					</slot>
+				</span>
+			</li>
 		</ul>
 
 		<form class="insert" @submit.prevent="insert">
 			<slot name="new">
 				<input class="input" type="text" v-model="newItem" />
+			</slot>
+
+			<slot name="submit">
+				<button type="submit">Add</button>
 			</slot>
 		</form>
 	</div>
@@ -20,25 +31,31 @@ export default {
 	watch: {
 		value: {
 			immediate: true,
-			handler(value){
-				this.$emit("input", value)
-			}
-		}
+			handler(value) {
+				this.$emit("input", value);
+			},
+		},
 	},
-	data(){
+	data() {
 		return {
 			newItem: "",
-		}
+		};
 	},
 	methods: {
-		insert(e){
-			this.value.push(this.newItem)
-			this.afterInsert(this.newItem, this.value)
+		insert(e) {
+			this.value.push(this.newItem);
+			this.afterInsert(this.newItem, this.value);
 		},
-		afterInsert(value, list){
+		remove(i) {
+			var list = this.value
+			list.splice(i, 1)
+			this.$emit("input", list)
+		},
+		afterInsert(value, list) {
 			this.newItem = "";
-		}
-	}
+		},
+		afterRemove(index, list) {},
+	},
 };
 </script>
 
