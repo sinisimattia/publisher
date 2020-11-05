6ad149e445a93cc709803e76181a465465a558e3
Mattia Sinisi <mattia@snisni.it>
2020-11-05 14:24:40 +0100

    Wrapper for title types

--- src/lib-components/Publisher.vue
+++ src/lib-components/Publisher.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="editor">
-    <editor-menu-bubble class="menububble" :editor="editor" @hide="hideLinkMenu" v-slot="{ commands, isActive, getMarkAttrs, menu }">
+    <editor-menu-bubble
+      class="menububble"
+      :editor="editor"
+      @hide="hideLinkMenu"
+      v-slot="{ commands, isActive, getMarkAttrs, menu }"
+    >
       <div
         class="menububble"
         :class="{ 'is-active': menu.isActive }"
         :style="`left: ${menu.left}px; bottom: ${menu.bottom}px;`"
       >
-
-        <form v-if="linkMenuIsActive" @submit.prevent="setLinkUrl(commands.link, linkUrl)">
-          <input class="input" type="search" v-model="linkUrl" placeholder="https://" ref="linkInput" @keydown.esc="hideLinkMenu"/>
+        <form
+          v-if="linkMenuIsActive"
+          @submit.prevent="setLinkUrl(commands.link, linkUrl)"
+        >
+          <input
+            class="input"
+            type="search"
+            v-model="linkUrl"
+            placeholder="https://"
+            ref="linkInput"
+            @keydown.esc="hideLinkMenu"
+          />
         </form>
 
         <template v-else>
@@ -22,49 +36,58 @@
             </span>
           </button>
         </template>
-
       </div>
     </editor-menu-bubble>
 
     <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
       <div class="commands">
-        <button
-          type="button"
-          :class="{ 'is-active': isActive.heading({level: 2}) }"
-          @click="commands.heading({level: 2})"
-        >
-          <slot name="title">
-            <span>Title</span>
-          </slot>
-        </button>
+        <span class="titles">
+          <button
+            type="button"
+            :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+            @click="commands.heading({ level: 2 })"
+          >
+            <slot name="title">
+              <span>Title</span>
+            </slot>
+          </button>
+
+          <button
+            type="button"
+            :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+            @click="commands.heading({ level: 3 })"
+          >
+            <slot name="subtitle">
+              <span>Subtitle</span>
+            </slot>
+          </button>
+
+          <button
+            type="button"
+            :class="{ 'is-active': isActive.heading({ level: 4 }) }"
+            @click="commands.heading({ level: 4 })"
+          >
+            <slot name="sectionTitle">
+              <span>Section Title</span>
+            </slot>
+          </button>
+        </span>
 
         <button
           type="button"
-          :class="{ 'is-active': isActive.heading({level: 3}) }"
-          @click="commands.heading({level: 3})"
+          :class="{ 'is-active': isActive.bold() }"
+          @click="commands.bold"
         >
-          <slot name="subtitle">
-            <span>Subtitle</span>
-          </slot>
-        </button>
-
-        <button
-          type="button"
-          :class="{ 'is-active': isActive.heading({level: 4}) }"
-          @click="commands.heading({level: 4})"
-        >
-          <slot name="sectionTitle">
-            <span>Section Title</span>
-          </slot>
-        </button>
-
-        <button type="button" :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
           <slot name="bold">
             <b>B</b>
           </slot>
         </button>
 
-        <button type="button" :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
+        <button
+          type="button"
+          :class="{ 'is-active': isActive.italic() }"
+          @click="commands.italic"
+        >
           <slot name="italic">
             <i>I</i>
           </slot>
@@ -80,10 +103,7 @@
           </slot>
         </button>
 
-        <button
-          type="button"
-          @click="imagePrompt(commands.image)"
-        >
+        <button type="button" @click="imagePrompt(commands.image)">
           <slot name="image">
             <span>IMG</span>
           </slot>
@@ -97,7 +117,14 @@
 
 <script>
 import { Editor, EditorContent, EditorMenuBar, EditorMenuBubble } from "tiptap";
-import { Bold, Italic, Blockquote, Heading, Link, Image } from "tiptap-extensions";
+import {
+  Bold,
+  Italic,
+  Blockquote,
+  Heading,
+  Link,
+  Image,
+} from "tiptap-extensions";
 
 export default {
   components: {
@@ -134,24 +161,24 @@ export default {
   },
   methods: {
     showLinkMenu(attrs) {
-      this.linkUrl = attrs.href
-      this.linkMenuIsActive = true
+      this.linkUrl = attrs.href;
+      this.linkMenuIsActive = true;
       this.$nextTick(() => {
-        this.$refs.linkInput.focus()
-      })
+        this.$refs.linkInput.focus();
+      });
     },
     hideLinkMenu() {
-      this.linkUrl = null
-      this.linkMenuIsActive = false
+      this.linkUrl = null;
+      this.linkMenuIsActive = false;
     },
     setLinkUrl(command, url) {
-      command({ href: url })
-      this.hideLinkMenu()
+      command({ href: url });
+      this.hideLinkMenu();
     },
-    imagePrompt(command){
-      const src = prompt('Enter the url of your image here')
+    imagePrompt(command) {
+      const src = prompt("Enter the url of your image here");
       if (src !== null) {
-        command({ src })
+        command({ src });
       }
     },
   },
@@ -160,9 +187,9 @@ export default {
       immediate: true,
       deep: true,
       handler(value) {
-        let { from, to } =(this.editor.state.selection)
+        let { from, to } = this.editor.state.selection;
         this.editor.setContent(value);
-        this.editor.setSelection(from, to)
+        this.editor.setSelection(from, to);
       },
     },
   },
