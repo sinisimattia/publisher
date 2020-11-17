a8ad04acafbc77fc3e5964e47d4c9a1f8e4e27d4
Mattia Sinisi <mattia@snisni.it>
2020-11-17 18:34:45 +0100

    Pass attributes to reader Node

--- src/custom-nodes/reader/IFrame.js
+++ src/custom-nodes/reader/IFrame.js
@@ -6,6 +6,14 @@ export default class IFrame extends Node {
     }
  
     tag () {
-        return "iframe";
+        return {
+            tag: "iframe",
+            attrs: {
+                "src": this.node.attrs.src,
+                "frameborder": this.node.attrs.frameborder,
+                "allowfullscreen": this.node.attrs.allowfullscreen,
+                "allow": this.node.attrs.allow,
+            }
+        };
     }
 }
