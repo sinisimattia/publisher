2f0cc14b7e664fe51d12abcbb51f2356468cda59
Mattia Sinisi <mattia@snisni.it>
2020-11-26 17:32:28 +0100

    Disable editing with a prop

--- src/lib-components/Publisher.vue
+++ src/lib-components/Publisher.vue
@@ -1,128 +1,132 @@
 <template>
-  <div class="editor">
-    <editor-menu-bubble
-      class="menububble"
-      :editor="editor"
-      @hide="hideLinkMenu"
-      v-slot="{ commands, isActive, getMarkAttrs, menu }"
-    >
-      <div
+  <div :class="{editor: editable, content: !editable}">
+    <div v-if="editable">
+      <editor-menu-bubble
         class="menububble"
-        :class="{ 'is-active': menu.isActive }"
-        :style="`left: ${menu.left}px; bottom: ${menu.bottom}px;`"
+        :editor="editor"
+        @hide="hideLinkMenu"
+        v-slot="{ commands, isActive, getMarkAttrs, menu }"
       >
-        <form
-          v-if="linkMenuIsActive"
-          @submit.prevent="setLinkUrl(commands.link, linkUrl)"
+        <div
+          class="menububble"
+          :class="{ 'is-active': menu.isActive }"
+          :style="`left: ${menu.left}px; bottom: ${menu.bottom}px;`"
         >
-          <input
-            class="input"
-            type="search"
-            v-model="linkUrl"
-            placeholder="https://"
-            ref="linkInput"
-            @keydown.esc="hideLinkMenu"
-          />
-        </form>
-
-        <template v-else>
-          <button
-            class="button"
-            @click="showLinkMenu(getMarkAttrs('link'))"
-            :class="{ 'is-active': isActive.link() }"
+          <form
+            v-if="linkMenuIsActive"
+            @submit.prevent="setLinkUrl(commands.link, linkUrl)"
           >
-            <span>
-              <slot name="link">Add link</slot>
-            </span>
-          </button>
-        </template>
-      </div>
-    </editor-menu-bubble>
+            <input
+              class="input"
+              type="search"
+              v-model="linkUrl"
+              placeholder="https://"
+              ref="linkInput"
+              @keydown.esc="hideLinkMenu"
+            />
+          </form>
 
-    <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
-      <div class="commands">
-        <span class="titles">
+          <template v-else>
+            <button
+              class="button"
+              @click="showLinkMenu(getMarkAttrs('link'))"
+              :class="{ 'is-active': isActive.link() }"
+            >
+              <span>
+                <slot name="link">Add link</slot>
+              </span>
+            </button>
+          </template>
+        </div>
+      </editor-menu-bubble>
+
+      <editor-menu-bar
+        :editor="editor"
+        v-slot="{ commands, isActive }"
+      >
+        <div class="commands">
+          <span class="titles">
+            <button
+              type="button"
+              :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+              @click="commands.heading({ level: 2 })"
+            >
+              <slot name="title">
+                <span>Title</span>
+              </slot>
+            </button>
+
+            <button
+              type="button"
+              :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+              @click="commands.heading({ level: 3 })"
+            >
+              <slot name="subtitle">
+                <span>Subtitle</span>
+              </slot>
+            </button>
+
+            <button
+              type="button"
+              :class="{ 'is-active': isActive.heading({ level: 4 }) }"
+              @click="commands.heading({ level: 4 })"
+            >
+              <slot name="sectionTitle">
+                <span>Section Title</span>
+              </slot>
+            </button>
+          </span>
+
           <button
             type="button"
-            :class="{ 'is-active': isActive.heading({ level: 2 }) }"
-            @click="commands.heading({ level: 2 })"
+            :class="{ 'is-active': isActive.bold() }"
+            @click="commands.bold"
           >
-            <slot name="title">
-              <span>Title</span>
+            <slot name="bold">
+              <b>B</b>
             </slot>
           </button>
 
           <button
             type="button"
-            :class="{ 'is-active': isActive.heading({ level: 3 }) }"
-            @click="commands.heading({ level: 3 })"
+            :class="{ 'is-active': isActive.italic() }"
+            @click="commands.italic"
           >
-            <slot name="subtitle">
-              <span>Subtitle</span>
+            <slot name="italic">
+              <i>I</i>
             </slot>
           </button>
 
           <button
             type="button"
-            :class="{ 'is-active': isActive.heading({ level: 4 }) }"
-            @click="commands.heading({ level: 4 })"
+            :class="{ 'is-active': isActive.blockquote() }"
+            @click="commands.blockquote"
           >
-            <slot name="sectionTitle">
-              <span>Section Title</span>
-            </slot>
-          </button>
-        </span>
-
-        <button
-          type="button"
-          :class="{ 'is-active': isActive.bold() }"
-          @click="commands.bold"
-        >
-          <slot name="bold">
-            <b>B</b>
-          </slot>
-        </button>
-
-        <button
-          type="button"
-          :class="{ 'is-active': isActive.italic() }"
-          @click="commands.italic"
-        >
-          <slot name="italic">
-            <i>I</i>
-          </slot>
-        </button>
-
-        <button
-          type="button"
-          :class="{ 'is-active': isActive.blockquote() }"
-          @click="commands.blockquote"
-        >
-          <slot name="blockquote">
-            <span>Q</span>
-          </slot>
-        </button>
-
-        <button type="button" @click="imagePrompt(commands.image)">
-          <slot name="image">
-            <span>IMG</span>
-          </slot>
-        </button>
-
-        <span>
-          <button type="button" @click="showVideoModal(commands.iframe)">
-            <slot name="embed">
-              <span>&lt;/&gt;</span>
+            <slot name="blockquote">
+              <span>Q</span>
             </slot>
           </button>
 
-          <URLModal ref="urlModal" @onConfirm="addCommand">
-            <slot name="createEmbed"></slot>
-          </URLModal>
-        </span>
-        
-      </div>
-    </editor-menu-bar>
+          <button type="button" @click="imagePrompt(commands.image)">
+            <slot name="image">
+              <span>IMG</span>
+            </slot>
+          </button>
+
+          <span>
+            <button type="button" @click="showVideoModal(commands.iframe)">
+              <slot name="embed">
+                <span>&lt;/&gt;</span>
+              </slot>
+            </button>
+
+            <URLModal ref="urlModal" @onConfirm="addCommand">
+              <slot name="createEmbed"></slot>
+            </URLModal>
+          </span>
+        </div>
+      </editor-menu-bar>
+    </div>
 
     <editor-content class="content" :editor="editor" />
   </div>
@@ -139,7 +143,7 @@ import {
   Image,
 } from "tiptap-extensions";
 import IFrame from "@/custom-nodes/IFrame";
-import URLModal from "@/internal-components/URLModal"
+import URLModal from "@/internal-components/URLModal";
 
 export default {
   components: {
@@ -150,10 +154,15 @@ export default {
   },
   props: {
     value: Object,
+    editable: {
+      type: Boolean,
+      default: true,
+    },
   },
   data() {
     return {
       editor: new Editor({
+        editable: this.editable,
         extensions: [
           new Bold(),
           new Italic(),
